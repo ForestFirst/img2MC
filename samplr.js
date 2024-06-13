25049e240a6695ef6de8c1346f05b06918f7a494
ForestFirst <89952667+ForestFirst@users.noreply.github.com>
2024-06-13 21:57:14 +0900

    Add files via upload

--- samplr.js
+++ samplr.js
@@ -307,10 +307,7 @@ function colorErrorDiffusion(img_data,imagecolors,processed_data,origin_xyz,zip,
             //比較
             var i = 0;
             let comp_hsvH = min_angle;
-            console.log("------------------------");
             while(i <= scope){
-                console.log(i);
-                console.log(comp_hsvH);
                 comp_value[i] = 0;
                 if(color_csv[0][comp_hsvH][0] != -1){
                     comp_value[i] = Math.abs(hsvS[index / 4][0] - color_csv[0][comp_hsvH][1]) * h_mag 
