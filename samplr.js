63aea1ca2ae278814bd436ee90750c822523f182
ForestFirst <89952667+ForestFirst@users.noreply.github.com>
2024-06-14 20:48:26 +0900

    Add files via upload

--- samplr.js
+++ samplr.js
@@ -133,11 +133,11 @@ function rgb2grey(img_data,imagecolors){
 色格納
 */
 function rgbInArray(img_data){
-    let color_data = [...Array(img_data.width * img_data.height * 4)].map(k=>0);
-    console.log(color_data);
+    let color_data = [];//[...Array(img_data.width * img_data.height * 4)].map(k=>0);
     for(var i = 0;i < img_data.width * img_data.height * 4;i += 4){
         for(var j = 0;j < 3;j++){
             color_data[i + j] = img_data.data[i + j];
+            console.log(color_data[i + j],img_data.data[i + j]);
         }
         color_data[i + 3] = 255;
     }
