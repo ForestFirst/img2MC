33ee4a04b86a1844af30591f4426bd2e7e717009
ForestFirst <89952667+ForestFirst@users.noreply.github.com>
2024-06-15 23:21:51 +0900

    Add files via upload

--- samplr.js
+++ samplr.js
@@ -372,19 +372,26 @@ function loadCSVFile(){
     var csv_array = [...Array(2)].map(k=>[...Array(360)].map(k=>[...Array(3)].map(k=>-1)));
     
     csv.open("get", "./BlocksColor.csv",true);
+    csv.withCredentials = true;
+    csv.send(null);
     
     csv.onload = function(){
-        //格納
-        let tmp_array = (csv.responseText).split("\n");
-        for(var i = 1;i < tmp_array.length - 1;i++){
-            let hsv_array = tmp_array[i].split(',').slice(7,10);
-            let rgb_array = tmp_array[i].split(',').slice(1,4);
-            csv_array[0][hsv_array[0]] = hsv_array.map( str => parseInt(str, 10));
-            console.log(csv_array[0][hsv_array[0]]);
-            //csv_array[0][hsv_array[0]] = [parseInt(hsv_array[0]),parseInt(hsv_array[1]),parseInt(hsv_array[2])];
-            csv_array[1][rgb_array[0]] = rgb_array.map( str => parseInt(str, 10));
-            console.log(csv_array[1][rgb_array[0]]);
-            //csv_array[1][hsv_array[0]] = [parseInt(rgb_array[0]),parseInt(rgb_array[1]),parseInt(rgb_array[2])];
+        if(csv.status == 200){
+            //格納
+            let tmp_array = (csv.responseText).split("\n");
+            for(var i = 1;i < tmp_array.length - 1;i++){
+                let hsv_array = tmp_array[i].split(',').slice(7,10);
+                let rgb_array = tmp_array[i].split(',').slice(1,4);
+                csv_array[0][hsv_array[0]] = hsv_array.map( str => parseInt(str, 10));
+                console.log(csv_array[0][hsv_array[0]]);
+                //csv_array[0][hsv_array[0]] = [parseInt(hsv_array[0]),parseInt(hsv_array[1]),parseInt(hsv_array[2])];
+                csv_array[1][rgb_array[0]] = rgb_array.map( str => parseInt(str, 10));
+                console.log(csv_array[1][rgb_array[0]]);
+                //csv_array[1][hsv_array[0]] = [parseInt(rgb_array[0]),parseInt(rgb_array[1]),parseInt(rgb_array[2])];
+            }
+        }
+        else{
+            console.log("ファイルエラー");
         }
     }
     return csv_array;
