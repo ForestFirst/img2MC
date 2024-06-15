398f6f1afbccda419064fdfc29bd80b5c2cef331
ForestFirst <89952667+ForestFirst@users.noreply.github.com>
2024-06-16 00:07:41 +0900

    Add files via upload

--- samplr.js
+++ samplr.js
@@ -379,12 +379,12 @@ function loadCSVFile(){
     for(var i = 1;i < tmp_array.length - 1;i++){
         let hsv_array = tmp_array[i].split(',').slice(7,10);
         let rgb_array = tmp_array[i].split(',').slice(1,4);
-        csv_array[0][hsv_array[0]] = hsv_array.map( str => parseInt(str, 10));
+        //csv_array[0][hsv_array[0]] = hsv_array.map( str => parseInt(str, 10));
+        csv_array[0][hsv_array[0]] = [parseInt(hsv_array[0]),parseInt(hsv_array[1]),parseInt(hsv_array[2])];
         console.log(csv_array[0][hsv_array[0]]);
-        //csv_array[0][hsv_array[0]] = [parseInt(hsv_array[0]),parseInt(hsv_array[1]),parseInt(hsv_array[2])];
-        csv_array[1][rgb_array[0]] = rgb_array.map( str => parseInt(str, 10));
+        //csv_array[1][rgb_array[0]] = rgb_array.map( str => parseInt(str, 10));
+        csv_array[1][rgb_array[0]] = [parseInt(rgb_array[0]),parseInt(rgb_array[1]),parseInt(rgb_array[2])];
         console.log(csv_array[1][rgb_array[0]]);
-        //csv_array[1][hsv_array[0]] = [parseInt(rgb_array[0]),parseInt(rgb_array[1]),parseInt(rgb_array[2])];
     }
     console.log(csv_array);
     return csv_array;
