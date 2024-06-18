0c62fb47c54d83f296b3729a46d82d21e052d3c8
ForestFirst <89952667+ForestFirst@users.noreply.github.com>
2024-06-18 20:15:10 +0900

    Add files via upload

--- sample_cie2000.js
+++ sample_cie2000.js
@@ -318,7 +318,8 @@ function angleSet(num,angle){
 function init_rgb2lab(array,array_size){
     let labS = [...Array(array_size)].map(k=>[...Array(3)].map(k=>-1));
     for(var i = 0;i < array_size * 4;i += 4){
-        labS[i / 4] = rgb2lab(array[i],array[i+1],array[i+2]).map((x) => Math.round(x));
+        tmp_labS = rgb2lab(array[i],array[i+1],array[i+2]).map((x) => Math.round(x));
+        labS[i / 4] = [Math.round(tmp_labS[0]),Math.round(tmp_labS[1]),Math.round(tmp_labS[2])];
     }
     return labS;
 }
