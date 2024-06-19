76ce863b211b0756e6aae593f4e7255cc551b2c0
ForestFirst <89952667+ForestFirst@users.noreply.github.com>
2024-06-19 20:37:58 +0900

    Add files via upload

--- sample_cie2000de.js
+++ sample_cie2000de.js
@@ -245,8 +245,8 @@ function colorErrorDiffusion(img_data,processed_data,origin_xyz,zip,folder){
             for(var i = 0;i < 3; i++){
                 //誤差（rgbそれぞれで算出）
                 //error[i] = output_data[index + i] - color_csv[1][comp_num][i];
-                error[i] = Math.sqrt(output_data[index + i] - color_csv[1][comp_num][i]);
-                
+                error[i] = output_data[index + i] - color_csv[1][comp_num][i];
+                if(error[i] < 26) error[i] = 0;
                 output_data[index + i] = color_csv[1][comp_num][i];
             }
 
