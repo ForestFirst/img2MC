2735dd08b3bbee2e62688994472258d863d0cd76
ForestFirst <89952667+ForestFirst@users.noreply.github.com>
2024-06-20 21:37:01 +0900

    Add files via upload

--- sample_cie2000de.js
+++ sample_cie2000de.js
@@ -521,28 +521,21 @@ function ciede2000(L1,a1,b1, L2,a2,b2) {
 /*
 最適化前
 */
-function ciede2000(L1,a1,b1, L2,a2,b2, kL=1,kC=1,kH=1) {
+function ciede2000(L1,a1,b1, L2,a2,b2) {
     //http://en.wikipedia.org/wiki/Color_difference#CIEDE2000
     var radianToDegree = function(radian) {return radian * (180 / Math.PI);};
     var degreeToRadian = function(degree) {return degree * (Math.PI / 180);};
 
-    var deltaLp = L2 - L1;
     var L_ = (L1 + L2) / 2;
     var C1 = Math.sqrt(Math.pow(a1, 2) + Math.pow(b1, 2));
     var C2 = Math.sqrt(Math.pow(a2, 2) + Math.pow(b2, 2));
     var C_ = (C1 + C2) / 2;
-    var ap1 = a1 + (a1 / 2) *
-        (1 - Math.sqrt(
-            Math.pow(C_, 7) /
-            (Math.pow(C_, 7) + Math.pow(25, 7))
-            )
-        );
-    var ap2 = a2 + (a2 / 2) *
-        (1 - Math.sqrt(
-            Math.pow(C_, 7) /
-            (Math.pow(C_, 7) + Math.pow(25, 7))
-            )
-        );
+    var pow_C_7 = Math.pow(C_, 7);
+    var pow_25_7 = Math.pow(25, 7);
+    var tmp_ap_calculate = (1 - Math.sqrt(pow_C_7 / (pow_C_7 + pow_25_7)));
+
+    var ap1 = a1 + (a1 / 2) * tmp_ap_calculate;
+    var ap2 = a2 + (a2 / 2) * tmp_ap_calculate;
     var Cp1 = Math.sqrt(Math.pow(ap1, 2) + Math.pow(b1, 2));
     var Cp2 = Math.sqrt(Math.pow(ap2, 2) + Math.pow(b2, 2));
     var Cp_ = (Cp1 + Cp2) / 2;
@@ -563,21 +556,22 @@ function ciede2000(L1,a1,b1, L2,a2,b2, kL=1,kC=1,kH=1) {
         if (hp2 < 0) {hp2 = hp2 + 360;}
     }
 
+    var distance_hp1_hp2 = hp2 - hp1;
     var deltahp;
     if (C1 == 0 || C2 == 0) {
         deltahp = 0;
-    } else if (Math.abs(hp1 - hp2) <= 180) {
-        deltahp = hp2 - hp1;
+    } else if (Math.abs(distance_hp1_hp2) <= 180) {
+        deltahp = distance_hp1_hp2;
     } else if (hp2 <= hp1) {
-        deltahp = hp2 - hp1 + 360;
+        deltahp = distance_hp1_hp2 + 360;
     } else {
-        deltahp = hp2 - hp1 - 360;
+        deltahp = distance_hp1_hp2 - 360;
     }
 
     var deltaHp = 2 * Math.sqrt(Cp1 * Cp2) * Math.sin(degreeToRadian(deltahp) / 2);
 
     var Hp_;
-    if (Math.abs(hp1 - hp2) > 180) {
+    if (Math.abs(distance_hp1_hp2) > 180) {
         Hp_ =  (hp1 + hp2 + 360) / 2
     } else {
         Hp_ = (hp1 + hp2) / 2
@@ -589,27 +583,26 @@ function ciede2000(L1,a1,b1, L2,a2,b2, kL=1,kC=1,kH=1) {
         0.32 * Math.cos(degreeToRadian(3 * Hp_ + 6)) -
         0.20 * Math.cos(degreeToRadian(4 * Hp_ - 63));
 
-    var SL = 1 + (
-        (0.015 * Math.pow(L_ - 50, 2)) /
-        Math.sqrt(20 + Math.pow(L_ - 50, 2))
-        );
+    var pow_50_2 = Math.pow(L_ - 50, 2);
+    var SL = 1 + ((0.015 * pow_50_2) / Math.sqrt(20 + pow_50_2));
     var SC = 1 + 0.045 * Cp_;
     var SH = 1 + 0.015 * Cp_ * T;
 
+    var pow_cp_7 = Math.pow(Cp_, 7);
     var RT = -2 *
         Math.sqrt(
-            Math.pow(Cp_, 7) /
-            (Math.pow(Cp_, 7) + Math.pow(25, 7))
+            pow_cp_7 /
+            (pow_cp_7 + pow_25_7)
         ) *
         Math.sin(degreeToRadian(
             60 * Math.exp(-Math.pow((Hp_ - 275) / 25, 2))
         ));
 
     return Math.sqrt(
-        Math.pow(deltaLp / (kL * SL), 2) +
-        Math.pow(deltaCp / (kC * SC), 2) +
-        Math.pow(deltaHp / (kH * SH), 2) +
-        RT * (deltaCp / (kC * SC)) * (deltaHp / (kH * SH))
+        Math.pow(L2 - L1 / SL, 2) +
+        Math.pow(deltaCp / SC, 2) +
+        Math.pow(deltaHp / SH, 2) +
+        RT * (deltaCp / SC) * (deltaHp / SH)
         );
 }
 
